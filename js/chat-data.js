bf92464373d1a6160113286cc73ffacefda25acc
DhaniCG <dhanigamer777@gmail.com>
2025-06-14 19:48:12 +0700

    first commit

---
+++ js/chat-data.js
@@ -0,0 +1,15 @@
+const chatData = [
+    "Hi 👋, I'm Emily from the Senior  Benefit Center",
+    "Want to find out if you qualify for the up to $5,000* Dental Allowance? Tap Yes! 😀",
+    {
+        options: ["Yes"],
+        answers: ["Are you on Medicare Parts A/B?"]
+    },
+    {
+        options: ["Yes", "No"],
+        answers: ["🎉 🎉 Great News!"]
+    },
+    "You are pre-qualified for a $5,000* Dental Allowance.",
+    "Tap the number below to call now and add it to your plan. It’s completely free and takes minutes to see what you can get.",
+    "Make sure to mention to the agent that you are looking for information on get a Dental Allowance for crowns, implants, and treatments."
+];
